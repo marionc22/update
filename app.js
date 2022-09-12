6d3d2390febd9b34d0d75b147ccd1d08aafbbea4
CORBEL Marion (SNCF VOYAGEURS / Direction Intercites / EQUIPE 1 ESCALE PAZ) <9301738k@commun.ad.sncf.fr>
2022-09-12 18:00:35 +0200

    token

--- app.js
+++ app.js
@@ -12,25 +12,34 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
-const { hashPassword } = require("./auth.js");
-
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
-
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+
+//Mur d'authentification
+
+app.use(verifyToken);
+
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+app.post("/api/movies", verifyToken,movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.listen(port, (err) => {
   if (err) {
@@ -39,3 +48,10 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+
+
+
+
+
+
